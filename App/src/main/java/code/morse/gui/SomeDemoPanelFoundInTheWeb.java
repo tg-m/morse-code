3fc9a97754692c33c876bfc84c6fa08e61d4ceb7
TGMarkiewicz <tomasz.g.markiewicz@gmail.com>
2016-08-22 18:55:23 +0200

    Add parent POM and Swing App

---
+++ App/src/main/java/code/morse/gui/SomeDemoPanelFoundInTheWeb.java
@@ -0,0 +1,62 @@
+package code.morse.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+class SomeDemoPanelFoundInTheWeb extends JPanel {
+    private int       squareX = 50;
+    private int       squareY = 50;
+    private final int squareW = 20;
+    private final int squareH = 20;
+
+    public SomeDemoPanelFoundInTheWeb() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(final MouseEvent e) {
+                moveSquare(e.getX(), e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(final MouseEvent e) {
+                moveSquare(e.getX(), e.getY());
+            }
+        });
+
+    }
+
+    private void moveSquare(final int x, final int y) {
+        final int OFFSET = 1;
+        if ((squareX != x) || (squareY != y)) {
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
+            squareX = x;
+            squareY = y;
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(250, 200);
+    }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+        g.drawString("This is my custom Panel!", 10, 20);
+        g.setColor(Color.RED);
+        g.fillRect(squareX, squareY, squareW, squareH);
+        g.setColor(Color.BLACK);
+        g.drawRect(squareX, squareY, squareW, squareH);
+    }
+}
